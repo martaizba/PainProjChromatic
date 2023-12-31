25a8ebb6fb49e244bd7bf07d238bb5f4d7a4f927
Marta Izquierdo Bayà <marta@Martas-MacBook-Air.local>
2023-12-31 20:34:07 +0100

    first commit

---
+++ src/Components/Button.jsx
@@ -0,0 +1,18 @@
+import "./Button.css";
+
+import React from "react";
+import PropTypes from "prop-types";
+
+
+const getStyles = (...args) => ["button", ...args].filter(Boolean).join(" ");
+
+const Button = ({ children, type }) => {
+  return <button className={getStyles(type)}>{children}</button>;
+};
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["primary", "secondary", "tertiary"]),
+};
+
+export default Button;
